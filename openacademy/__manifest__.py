a13a9b7bb81048874e4caa0f12f6b088fc78793f
Ger31misael <gersonfunez43@gmail.com>
2021-02-14 21:21:31 -0600

    [ADD] agregando linea templates en manifest Ps E.

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,6 +26,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'templates.xml',
         'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
