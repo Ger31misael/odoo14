34134b242f7c38d41d8151501044a5aad3cd91b6
Ger31misael <gersonfunez43@gmail.com>
2021-02-15 21:47:21 -0600

    [ADD] Herencia

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -29,6 +29,7 @@
         'views/openacademy.xml',
         'views/course.xml',
         'views/session.xml',
+        'views/partner.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
